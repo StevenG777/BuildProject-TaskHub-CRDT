8b3b75d70a35f42ed119e7dd0be5ad861ccc7c90
Steven <96842497+StevenG777@users.noreply.github.com>
2025-05-28 17:15:32 -0700

    Delete server/serverCredential.js

--- server/serverCredential.js
+++
@@ -1,5 +0,0 @@
-// Get the Y-Sweet Server WS link -> Connection String From Y-Sweet Cloud Dashboard
-const CONNECTION_STRING = "yss://nAAhatM_LzAPrhVXgY8.AAAgYzG1jY1dYJO0wTg-pLJrujq8Nh0J-C4Obnjvz8TqiVY@y-sweet.net/p/cdMJQH9ObkXc-m0x8nU/";
-
-// Export the credential
-export { CONNECTION_STRING };
